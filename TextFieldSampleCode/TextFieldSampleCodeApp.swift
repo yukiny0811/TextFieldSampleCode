a82808082d90c3e8a1a143519b6549635924b0ac
yukiny0811 <yukiny0811@gmail.com>
2024-06-19 18:37:19 +0900

    Initial Commit

---
+++ TextFieldSampleCode/TextFieldSampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldSampleCodeApp.swift
+//  TextFieldSampleCode
+//
+//  Created by Yuki Kuwashima on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldSampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
